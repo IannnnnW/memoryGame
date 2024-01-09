4ac0ff4550844b6f7f02d1be2beadfa397552007
IannnnnW <ianwamai@gmail.com>
2024-01-09 11:25:46 +0300

    Added mobile responsiveness

--- src/Components/GameOverComponent/GameOver.jsx
+++ src/Components/GameOverComponent/GameOver.jsx
@@ -4,25 +4,10 @@ import Button from "../ButtonComponent/Button";
 import './GameOver.css'
 
 function GameOver({isOpen, resetFunction, score, emojiCount, changeLevel}){
-  const style={
-    content: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      textAlign: 'center',
-      position: 'absolute',
-      minWidth: '50vw',
-      left: '50%',
-      top: '50%',
-      transform: 'translate(-50%, -50%)',
-      background: '#fff',
-      borderRadius: '30px',
-    }
-  }
+
   return(
-    <ReactModal isOpen={isOpen} style={style} ariaHideApp={false} portalClassName="modal">
-      {score == emojiCount ? <h4 className="winningText">WOW &#128525;, You won!</h4> : <h4 className="losingText">You Lose &#128517;, Better Luck Next Time!</h4>}
+    <ReactModal isOpen={isOpen} className="modal" ariaHideApp={false} portalClassName="modal">
+      {score == emojiCount ? <p className="winningText">WOW &#128525;, You won!</p> : <p className="losingText">You Lose &#128517;, Better Luck Next Time!</p>}
         <div className="functionButtons">
           <Button label="Play Again?" onButtonClick={resetFunction} emoji="&#128018;"/>
           <Button label="Change Level" onButtonClick={changeLevel} emoji="&#9197;"/>
